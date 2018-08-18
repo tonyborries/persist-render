3d415dc3d351b18acbf9d6768c3f0db4fa4b8a6e
TonyB <hiretonyb@gmail.com>
2018-08-18 17:43:53 -0500

    Refactor and fixup filename consistency.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import csv
 import os.path
 from tabulate import tabulate
 
-import persist 
+import persist
 
 
 BASE_DIR="/opt/persist/media/"
@@ -11,7 +11,7 @@ BASE_DIR="/opt/persist/media/"
 ###
 # Main Process - not an async task itself
 
-def process_video(rel_source_filename, persist_frames, skip_level=False, infinite_persist=False):
+def process_video(rel_source_filename, persist_frames, skip_level, infinite_persist, speedup):
     """
     rel_source_filename is relative to BASE_DIR
     """
@@ -19,33 +19,44 @@ def process_video(rel_source_filename, persist_frames, skip_level=False, infinit
     print "============\nProcessing %s - Persist %d frames\n" % (rel_source_filename, persist_frames)
 
     project_name = ''.join(x for x in rel_source_filename if x.isalnum())
-    output_name = "{}-persisted-{}{}{}.mp4".format(
+    output_name = "{}-persisted-{}{}{}{}.mp4".format(
         project_name,
         persist_frames,
         'sl' if skip_level else '',
-        'i' if infinite_persist else ''
+        'i' if infinite_persist else '',
+        '{}x'.format(speedup) if speedup > 1 else ''
         )
 
     if os.path.isfile(os.path.join(BASE_DIR, output_name)):
         print "File Exists - skipping: %s" % output_name
         return
 
+    p = persist.Persist(
+        input_filename = rel_source_filename,
+        output_filename = output_name,
+        persisted_frames = persist_frames,
+        skip_level = skip_level,
+        infinite_persist = infinite_persist,
+        speedup = speedup,
+    )
+
+
     print "Extracting Frames..."
-    t = persist.split_video_into_frames.delay(project_name, rel_source_filename, persist_frames)
+    t = persist.split_video_into_frames.delay(rel_source_filename, persist_frames, p.getSerializedDict())
     result = t.get()
     frames = result['frames']
     print "Completed Frame Extraction - %d frames - %s\n" % (frames, result.get('status', "UNKNOWN"))
 
     print "Converting Frames..."
     res = group(
-        persist.generate_persisted_frame.s(project_name, persist_frames, i, 1, frames, skip_level=skip_level, infinite_persist=infinite_persist)
+        persist.generate_persisted_frame.s(p.getSerializedDict(), rel_source_filename, persist_frames, i, 1, frames, skip_level=skip_level, infinite_persist=infinite_persist)
         for i in xrange(1, frames + 1)
     )()
     res.get()
     print "Done Converting Frames\n"
 
     print "Assembling Frames..."
-    t = persist.assemble_frames_into_video.delay(project_name, persist_frames, output_name)
+    t = persist.assemble_frames_into_video.delay(rel_source_filename, persist_frames, skip_level, infinite_persist, output_name, p.getSerializedDict())
     result = t.get()
     print "Assembled: %s" % result
 
@@ -57,7 +68,7 @@ def speedup_video(rel_source_filename, x_times):
     """
     Speedup a video by merging x_times of frames together into a single
     frame - does not do any persistence
-    
+
     rel_source_filename is relative to BASE_DIR
     """
 
@@ -66,26 +77,36 @@ def speedup_video(rel_source_filename, x_times):
     project_name = ''.join(x for x in rel_source_filename if x.isalnum()) + 'speed'
     output_name = project_name + "-speed-%dx.mp4" % x_times
 
-    if os.path.isfile(output_name):
+    if os.path.isfile(os.path.join(BASE_DIR, output_name)):
         print "File Exists - skipping: %s" % output_name
         return output_name
 
+    p = persist.Persist(
+        input_filename = rel_source_filename,
+        output_filename = output_name,
+        persisted_frames = 0,
+        skip_level = False,
+        infinite_persist = False,
+        speedup = x_times,
+    )
+
+
     print "Extracting Frames..."
-    t = persist.split_video_into_frames.delay(project_name, rel_source_filename, 0)
+    t = persist.split_video_into_frames.delay(rel_source_filename, 0, p.getSerializedDict())
     result = t.get()
     frames = result['frames']
     print "Completed Frame Extraction - %d frames - %s\n" % (frames, result.get('status', "UNKNOWN"))
 
     print "Converting Frames..."
     res = group(
-        persist.merge_frames.s(project_name, i, i * x_times, (i + 1) * x_times, 0)
+        persist.merge_frames.s(i, i * x_times, (i + 1) * x_times, p.getSerializedDict())
         for i in xrange(1, int(frames / x_times))
     )()
     res.get()
     print "Done Converting Frames\n"
 
     print "Assembling Frames..."
-    t = persist.assemble_frames_into_video.delay(project_name, 0, output_name)
+    t = persist.assemble_frames_into_video.delay(rel_source_filename, 0, False, False, output_name, p.getSerializedDict())
     result = t.get()
     print "Assembled: %s" % result
 
@@ -93,25 +114,6 @@ def speedup_video(rel_source_filename, x_times):
     print "Completed %s\n============" % rel_source_filename
 
     return output_name
-
-
-
-#f = "0505dark.MOV"
-#process_video(f, persist_frames=5)
-#process_video(f, persist_frames=25)
-#process_video(f, persist_frames=100)
-#process_video(f, persist_frames=250)
-#process_video(f, persist_frames=500)
-#process_video(f, persist_frames=1000)
-#process_video(f, persist_frames=10000)
-
-
-#f = 'DSC_0532-average-hack.MOV'
-#f = speedup_video(f, 60)
-#process_video(f, persist_frames=1, skip_level=True, infinite_persist=False)
-
-#f = "DSC_0502.MOV"
-#process_video(f, persist_frames=1, skip_level=True, infinite_persist=True)
 
 
 CSV_FILENAME = 'media/config.csv'
@@ -134,8 +136,8 @@ if __name__ == "__main__":
         infile, speedup, persist_frames, skip_level, infinite_persist = task
         speedup = int(speedup)
         persist_frames = int(persist_frames)
-        skip_level = bool(skip_level)
-        infinite_persist = bool(infinite_persist)
+        skip_level = bool(int(skip_level))
+        infinite_persist = bool(int(infinite_persist))
 
         f = infile
         if speedup != 1:
@@ -143,12 +145,13 @@ if __name__ == "__main__":
             f = speedup_video(f, speedup)
 
         if persist_frames > 0:
-            print "Process {} {} {} {}".format(infile, persist_frames, skip_level, infinite_persist)
+            print "Process {} {} {} {}".format(infile, persist_frames, skip_level, infinite_persist, speedup)
             process_video(
                 infile,
                 persist_frames=persist_frames,
                 skip_level=skip_level,
-                infinite_persist=infinite_persist
+                infinite_persist=infinite_persist,
+                speedup=speedup
             )
         completed_tasks.append((
             infile,
